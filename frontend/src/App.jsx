a4409f89a32512c957862b8d9667a978243e5878
ARVIND KUMAR <arvindnisha2002@gmail.com>
2024-04-08 03:55:10 +0530

    --

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,29 @@
+import {Navigate, Route,Routes} from "react-router-dom"
+import './App.css';
+import Home from './pages/home/Home';
+import Login from "./pages/login/Login";
+import Signup from './pages/signup/Signup';
+import {Toaster} from "react-hot-toast"
+import { useAuthContext } from "./context/AuthContext";
+import bg1 from "../public/bg1.mp4";
+
+function App() {
+  const {authUser} = useAuthContext();
+  return <div className='p-4 h-screen flex items-center justify-center'>
+       <Routes>
+          <Route path="/" element={authUser ? <Home/> : <Navigate to={"/login"} />} />
+          <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login/>} />
+          <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup/>} />
+       </Routes>
+       <Toaster/>
+    </div>
+}
+
+export default App
+
+
+
+
 // import { Navigate, Route, Routes } from "react-router-dom";
 // import './App.css';
 // import Home from './pages/home/Home';
@@ -27,27 +53,3 @@
 // }
 
 // export default App;
-
-
-import {Navigate, Route,Routes} from "react-router-dom"
-import './App.css';
-import Home from './pages/home/Home';
-import Login from "./pages/login/Login";
-import Signup from './pages/signup/Signup';
-import {Toaster} from "react-hot-toast"
-import { useAuthContext } from "./context/AuthContext";
-import bg1 from "../public/bg1.mp4";
-
-function App() {
-  const {authUser} = useAuthContext();
-  return <div className='p-4 h-screen flex items-center justify-center'>
-       <Routes>
-          <Route path="/" element={authUser ? <Home/> : <Navigate to={"/login"} />} />
-          <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login/>} />
-          <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup/>} />
-       </Routes>
-       <Toaster/>
-    </div>
-}
-
-export default App
