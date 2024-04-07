96c660a39aa80686aefe5c879cee80073c2884a9
ARVIND KUMAR <arvindnisha2002@gmail.com>
2024-04-08 05:08:42 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() =>{
 		if (authUser) {
-			const socket = io("http://localhost:8000",{  // use render hosted link
+			const socket = io("https://chat-application-4krm.onrender.com/",{  // use render hosted link
                 query: {
                     userId:authUser._id,
                 },
