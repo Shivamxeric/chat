4a081732a01544781199e6a36a0ca55cf28cccea
Shivam Jha <sjhaxkira@gmail.com>
2025-03-01 13:07:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://chat-app-mern-stack-jf9b.onrender.com",
+				changeOrigin: true,
+				secure: false, // <-- Add this line
 			},
 		},
 	},
